c2eac130b608de24c9287018dc9d65f750d30543
Thiago Galbiatti Vespa <thiagovespa@gvlabs.org>
2012-11-05 16:40:40 -0200

    Initial import

---
+++ gvlabs-soap-utils-test/src/org/gvlabs/utils/soap/SoapClientTest.java
@@ -0,0 +1,42 @@
+package org.gvlabs.utils.soap;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SoapClientTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInvokeOperation() {
+		String endpoint = "http://www.webservicex.net/ConvertWeight.asmx";
+		// String endpoint = "http://localhost:8088/mockConvertWeightsSoap";
+		String operation = "http://www.webserviceX.NET/ConvertWeight";
+		String input = "<web:ConvertWeight xmlns:web=\"http://www.webserviceX.NET/\">"
+				+ "<web:Weight>7</web:Weight>"
+				+ "<web:FromUnit>Grains</web:FromUnit>"
+				+ "<web:ToUnit>Grams</web:ToUnit>" + "</web:ConvertWeight>";
+
+		SoapClient soapClient = new SoapClient(endpoint);
+
+		String response = null;
+		try {
+			response = soapClient.invokeOperation(operation, input);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><ConvertWeightResponse xmlns=\"http://www.webserviceX.NET/\"><ConvertWeightResult>0.4536</ConvertWeightResult></ConvertWeightResponse></soap:Body></soap:Envelope>", response);
+		
+	}
+
+}
